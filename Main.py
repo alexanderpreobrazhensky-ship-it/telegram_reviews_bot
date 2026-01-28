20e245a40c9b4f8fe7936a1d84894cb1fce4a443
alexanderpreobrazhensky-ship-it <alexanderpreobrazhensky@yahoo.com>
2026-01-28 09:31:27 +0300

    Add Telegram bot functionality to Main.py
    Signed-off-by: alexanderpreobrazhensky-ship-it <alexanderpreobrazhensky@yahoo.com>

--- Main.py
+++ Main.py
@@ -1,1 +1,12 @@
-print("Hello world")
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = "7917601350:AAFG1E7kHKrNzTXIprNADOzLvxpnrUjAcO4"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Бот работает 👍")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
