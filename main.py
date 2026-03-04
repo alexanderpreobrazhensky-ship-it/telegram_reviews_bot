5981d7b41e67d5adf9368e120b6ed5d2be424563
alexanderpreobrazhensky-ship-it <alexanderpreobrazhensky@yahoo.com>
2026-03-04 09:09:09 +0300

    Delete main.py
    Signed-off-by: alexanderpreobrazhensky-ship-it <alexanderpreobrazhensky@yahoo.com>

--- main.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-from services.client_bot_service.app.main import main as client_main
-
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
-    logging.getLogger("entrypoint").info("client-bot starting (root main.py)")
-    client_main()
